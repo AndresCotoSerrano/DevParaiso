6b746d56ee32340e4ea07fc51330077a254abf98
BrayanGS <31012783+BrayanGS@users.noreply.github.com>
2018-09-22 14:36:52 -0600

    Attendant Dao

---
+++ devparaiso/src/main/java/retoprogramathon2018/devparaiso/data/AttendantDao.java
@@ -0,0 +1,107 @@
+package retoprogramathon2018.devparaiso.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.stereotype.Repository;
+import retoprogramathon2018.devparaiso.domain.Attendant;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class AttendantDao {
+
+    private JdbcTemplate jdbcTemplate;
+    private DataSource dataSource;
+    private SimpleJdbcCall simpleJdbcCall;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.simpleJdbcCall = new SimpleJdbcCall(dataSource);
+    }
+
+    public Attendant Insert(Attendant attendant) throws SQLException {
+        Connection connection = dataSource.getConnection();
+        String sqlInsert = "{call DEV_AttendantInsert(?,?,?,?,?,?,?)}";
+
+        CallableStatement statement = connection.prepareCall(sqlInsert);
+
+        statement.registerOutParameter(1, Types.INTEGER);
+
+
+        statement.setString(2, attendant.getAttendantName());
+        statement.setString(3, attendant.getAttendantLastName());
+        statement.setString(4, attendant.getIdNumber());
+        statement.setString(5, attendant.getPhoneNumber());
+        statement.setString(6, attendant.getEmail());
+        statement.setString(7, attendant.getPassword());
+        statement.execute();
+        attendant.setIdNumber(statement.getString("user_name"));
+        statement.close();
+        connection.close();
+        return attendant;
+    }
+
+    public List<Attendant> Login(String email, String password){
+        System.out.println(email);
+        String sqlSelect = "Select *from Attendant";
+        return jdbcTemplate.query(sqlSelect, new LoginExtract());
+    }
+
+    public static class LoginExtract implements ResultSetExtractor<List<Attendant>>{
+        @Override
+        public List<Attendant> extractData(ResultSet rs) throws SQLException, DataAccessException {
+            Map<Integer, Attendant> map = new HashMap<>();
+            Attendant attendant = null;
+            while(rs.next()){
+                Integer idAttendant = rs.getInt("id_attendant");
+                attendant = map.get(idAttendant);
+
+                if(attendant == null) {
+
+                    attendant = new Attendant();
+                    attendant.setAttendantName(rs.getString("attendant_name"));
+                    attendant.setAttendantLastName(rs.getString("attendant_lastname"));
+                    attendant.setIdNumber(rs.getString("id_number"));
+                    attendant.setPhoneNumber(rs.getString("phone_number"));
+                    attendant.setEmail(rs.getString("email"));
+                    attendant.setPassword(rs.getString("password"));
+
+                    map.put(idAttendant, attendant);
+                }
+            }
+
+            return new ArrayList<Attendant>(map.values());
+        }
+    }
+
+    public Boolean AttendantVerify(Attendant attendant) throws SQLException {
+        Connection connection = dataSource.getConnection();
+        String sqlVerify = "{call DEV_AttendantVerify(?,?)}";
+        CallableStatement statement = connection.prepareCall(sqlVerify);
+        statement.setString(1, attendant.getEmail());
+        statement.setString(2, attendant.getPassword());
+        statement.execute();
+
+        String email = statement.getString("email");
+        String password = statement.getString("password");
+
+        if(attendant.getEmail() == email && attendant.getPassword() == password){
+            System.out.println("Attendant: " + attendant.getEmail() + "\nPassword: " + attendant.getPassword());
+            return true;
+        }
+        statement.close();
+        connection.close();
+
+        return false;
+    }
+}
--- devparaiso/src/main/java/retoprogramathon2018/devparaiso/domain/Attendant.java
+++ devparaiso/src/main/java/retoprogramathon2018/devparaiso/domain/Attendant.java
@@ -1,16 +1,21 @@
 package retoprogramathon2018.devparaiso.domain;
 
 
+
 public class Attendant {
 
-    private String userName;
+    private int userCount ;
+    private String attendantName;
+    private String attendantLastName;
     private String idNumber;
     private String phoneNumber;
     private String email;
     private String password;
 
-    public Attendant(String userName, String idNumber, String phoneNumber, String email, String password) {
-        this.userName = userName;
+    public Attendant(int userCount, String attendantName, String attendantLastName, String idNumber, String phoneNumber, String email, String password) {
+        this.userCount = userCount;
+        this.attendantName = attendantName;
+        this.attendantLastName = attendantLastName;
         this.idNumber = idNumber;
         this.phoneNumber = phoneNumber;
         this.email = email;
@@ -18,14 +23,6 @@ public class Attendant {
     }
 
     public Attendant() {
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
     }
 
     public String getIdNumber() {
@@ -58,5 +55,29 @@ public class Attendant {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getUserCount() {
+        return userCount;
+    }
+
+    public void setUserCount(int userCount) {
+        this.userCount = userCount;
+    }
+
+    public String getAttendantName() {
+        return attendantName;
+    }
+
+    public void setAttendantName(String attendantName) {
+        this.attendantName = attendantName;
+    }
+
+    public String getAttendantLastName() {
+        return attendantLastName;
+    }
+
+    public void setAttendantLastName(String attendantLastName) {
+        this.attendantLastName = attendantLastName;
     }
 }
