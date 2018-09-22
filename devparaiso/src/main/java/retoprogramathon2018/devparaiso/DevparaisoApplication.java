574574d3dc21d0eb8fd3995bcc1239b4fda8f8b1
Andres Coto <andres.coto3539@gmail.com>
2018-09-22 11:14:35 -0600

    base del proyecto

---
+++ devparaiso/src/main/java/retoprogramathon2018/devparaiso/DevparaisoApplication.java
@@ -0,0 +1,12 @@
+package retoprogramathon2018.devparaiso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevparaisoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevparaisoApplication.class, args);
+	}
+}
